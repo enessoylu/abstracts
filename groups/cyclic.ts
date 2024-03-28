eed34ad3bdfd246a2c936800695280cc23ad56a8
Enes Soylu <enessoylu@eness-mbp.speedport_w_723v_1_49_000>
2024-03-29 00:30:18 +0100

    use object instead of class for cyclic

--- groups/cyclic.ts
+++ groups/cyclic.ts
@@ -3,18 +3,18 @@ import { range } from "../utils";
 
 // any number from 0...n which is relatively prime with n generates order n cyclic group
 const CyclicGroupOf = (n: number): Group<number> => {
-  const list = range(n);
+  const set = range(n);
   const e = 0;
 
   const mul = (s: number, t: number) => (s + t) % n
   const inverse = (s: number) => n - s
 
-  return new (class implements Group<number> {
-    inverse = inverse;
-    e = e;
-    set = list;
-    mul = mul;
-  })
+  return {
+    inverse,
+    e,
+    set,
+    mul,
+  }
 }
 
 export { CyclicGroupOf }
