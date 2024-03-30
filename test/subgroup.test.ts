5823d87b2c12fe8f28a66a664addb63c90ef268a
Enes Soylu <enessoylu@eness-mbp.speedport_w_723v_1_49_000>
2024-03-30 17:07:13 +0100

    subgroup test fix

--- test/subgroup.test.ts
+++ test/subgroup.test.ts
@@ -1,39 +1,27 @@
-import { Permutation, getPermInverse, getPermMul, permute, printTable } from "../groups"
+import { fromCycles, inverseOfPermutation, multiply, printTable, toCycleString } from "../groups"
+import { SymmetricGroupOf } from "../groups/symmetric";
 import { Group } from "../types";
 
 test('Subgroups', () => {
-  const perm3 = permute(3)
+  const S3 = SymmetricGroupOf(3);
 
-  const items = perm3.map(p => new Permutation(p))
-  const e = items.find(p => p.toString() === '')
-  const mul = getPermMul(items)
-  const inverse = getPermInverse(items, mul);
-
-  const s3: Group<Permutation> = new (class implements Group<Permutation> {
-    set = items;
-    e = e
-
-    inverse = inverse
-    mul = mul
-  })
-
-  printTable(s3)
+  printTable(S3)
 
   const sub_items = [
-    Permutation.fromCycles('(1,2,3)', 3),
-    Permutation.fromCycles('(1,3,2)', 3),
-    Permutation.fromCycles('', 3),
+    '(1,2,3)',
+    '(1,3,2)',
+    '',
   ]
-  const sub_mul = getPermMul(sub_items)
-  const sub_inverse = getPermInverse(sub_items, sub_mul);
-  const sub_e = sub_items.find(p => p.toString() === '');
-  const sub_s3: Group<Permutation> = new (class implements Group<Permutation> {
-    e = sub_e
-    set = sub_items
+  const sub_mul = (a: string, b: string) => toCycleString(multiply(fromCycles(a), fromCycles(b)))
 
-    inverse = sub_inverse;
-    mul = sub_mul
-  })
+  const sub_inverse = inverseOfPermutation
+  const sub_e = '';
+  const subS3: Group<string> = {
+    e: sub_e,
+    inverse: sub_inverse,
+    mul: sub_mul,
+    set: sub_items
+  }
 
-  printTable(sub_s3)
+  printTable(subS3)
 })
