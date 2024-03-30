853b1ddeeb6ca0eeecd2cc8b89162d20f646ab09
Enes Soylu <enessoylu@eness-mbp.speedport_w_723v_1_49_000>
2024-03-30 16:15:01 +0100

    symmetric group implementation

---
+++ groups/symmetric.ts
@@ -0,0 +1,22 @@
+import { Group } from "../types";
+import { fromCycles, toCycleString, permute, multiply, inverseOfPermutation } from "./permutation";
+
+const SymmetricGroupOf = (n: number): Group<string> => {
+  const e = '';
+  const mul = (a: string, b: string) => toCycleString(multiply(fromCycles(a), fromCycles(b)))
+  const inverse = (a: string) => inverseOfPermutation(a)
+  const set = permute(n).map(list => toCycleString(list).toString());
+
+
+  return {
+    e,
+    set,
+    inverse,
+    mul,
+  }
+}
+
+export {
+  SymmetricGroupOf
+}
+
---
+++ test/symmetric.test.ts
@@ -0,0 +1,24 @@
+import { SymmetricGroupOf } from "../groups/symmetric"
+
+test('Symmetric', () => {
+  const S3 = SymmetricGroupOf(3);
+
+  expect(S3.set).toMatchObject([
+    '',
+    "(2,3)",
+    "(1,2)",
+    "(1,2,3)",
+    "(1,3,2)",
+    "(1,3)"
+  ])
+  expect(S3.e).toBe('')
+  
+  expect(S3.mul('', '(1,2)')).toBe('(1,2)')
+  expect(S3.mul('(2,3)', '(1,2)')).toBe('(1,2,3)')
+  expect(S3.mul('(2,3)', '(2,3)')).toBe('')
+
+  expect(S3.inverse('')).toBe('')
+  expect(S3.inverse('(1,2)')).toBe('(1,2)')
+  expect(S3.inverse('(1,2,3)')).toBe('(1,3,2)')
+  expect(S3.inverse('(1,3,2)')).toBe('(1,2,3)')
+})
