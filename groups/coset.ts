85b01b1056a3b0ba931a847c879efb4e77c60295
Enes Soylu <enessoylutr@gmail.com>
2024-04-06 12:21:07 +0200

    implement cosets

---
+++ groups/coset.ts
@@ -0,0 +1,27 @@
+import { Coset, Group } from "../types/group"
+import { uniqueSets } from "./utils"
+
+/** gH = {gh : h an element of H} for g in G. */
+const leftCosetOf = <T>(H: Group<T>, g: T): Coset<T> => {
+  return H.set.map(h => H.mul(h, g))
+}
+
+/** Hg = {hg : h an element of H} for g in G. */
+const rightCosetOf = <T>(H: Group<T>, g: T): Coset<T> => {
+  return H.set.map(h => H.mul(g, h))
+}
+
+/** The index of H in G, written [G : H], is the number of left (or right) cosets of H in G if this number is finite. */
+const indexOfSubgroup =  <T>(G: Group<T>, H: Group<T>) => {
+  const leftCosets = G.set.map(g => leftCosetOf(H, g))
+
+  // any two cosets are either identical or disjoint.
+  const uniqueLeftCosets = uniqueSets(leftCosets)
+  return uniqueLeftCosets.length;
+}
+
+export {
+  leftCosetOf,
+  rightCosetOf,
+  indexOfSubgroup,
+}
---
+++ test/coset.test.ts
@@ -0,0 +1,44 @@
+import { expect, test } from "vitest";
+import { SymmetricGroupOf } from "../groups/symmetric";
+import { indexOfSubgroup, leftCosetOf, rightCosetOf } from "../groups/coset";
+import { fromGenerator } from "../groups/group";
+import { areDisjointSets, isSetFormedByPartitions, uniqueSets } from "../groups/utils";
+
+// Let G be a group and H a subgroup. Then the left cosets of H in G partition G
+test('left cosets of H partition G', () => {
+  const G = SymmetricGroupOf(4)
+  const H = fromGenerator(['(1,2)'], G.mul, G.inverse)
+
+  const leftCosets = G.set.map(g => leftCosetOf(H, g))
+
+  // any two cosets are either identical or disjoint.
+  const uniqueLeftCosets = uniqueSets(leftCosets)
+
+  // G is the disjoint union of the left cosets gH for all g œ G.
+  expect(areDisjointSets(uniqueLeftCosets)).toBe(true);
+
+  expect(isSetFormedByPartitions(G.set, uniqueLeftCosets))
+})
+
+test('number of left and right cosets are equal', () => {
+  const G = SymmetricGroupOf(4)
+  const H = fromGenerator(['(1,2)'], G.mul, G.inverse)
+
+  const leftCosets = G.set.map(g => leftCosetOf(H, g))
+  const rightCosets = G.set.map(g => rightCosetOf(H, g))
+
+  const uniqueLeftCosets = uniqueSets(leftCosets)
+  const uniqueRightCosets = uniqueSets(rightCosets)
+
+  expect(uniqueLeftCosets.length).toBe(uniqueRightCosets.length)
+})
+
+// |G| = |H|[G : H]
+test('order of set equals to order of subgroup times index of subgroup', () => {
+  const G = SymmetricGroupOf(4)
+  const H = fromGenerator(['(1,2)'], G.mul, G.inverse)
+
+  const index = indexOfSubgroup(G, H);
+
+  expect(G.set.length).toBe(H.set.length * index)
+})
